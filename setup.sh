5ae79dd2e18a2e4dded7981fd0b3a9bbb7fbcc20
Wildan M <willnode@wellosoft.net>
2025-06-08 16:48:46 +0700

    Fix cargo path

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 git clone https://gitlab.redox-os.org/willnode/redox -b faster-libtool-clone --depth 1
 (cd redox && git submodule update --init --recursive --depth 1)
-export PATH=~/.cargo:$PATH
+export PATH=$HOME/.cargo/bin:$PATH
 . redox/podman/rustinstall.sh
 cp config ./redox/.config
 (cd redox && make build/x86_64/base/repo.tag)
