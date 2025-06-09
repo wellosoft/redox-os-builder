8f1c1fa96148c077fffbcd825c7007b814a2897b
Wildan M <willnode@wellosoft.net>
2025-06-09 07:14:40 +0700

    Write keys

--- setup.sh
+++ setup.sh
@@ -7,5 +7,14 @@ export PATH=$HOME/.cargo/bin:$PATH
 . redox/podman/rustinstall.sh
 cp config ./redox/.config
 cp -a build ./redox/build
+
+if [ -n "$PRIVATE_KEY" ] && [ -n "$PUBLIC_KEY" ]; then
+  printf "%s" "$PRIVATE_KEY" > "redox/cookbook/build/id_ed25519.toml"
+  printf "%s" "$PUBLIC_KEY" > "redox/cookbook/build/id_ed25519.pub.toml"
+else
+  echo "PRIVATE_KEY or PUBLIC_KEY is not set! Skipping key write."
+fi
+
 # must sync with config: build/<ARCH>/<CONFIG>/repo.tag
-(cd redox && make build/x86_64/dev/repo.tag || true)
+(cd redox && make build/x86_64/dev/repo.tag)
+cp -a redox/cookbook/build/id_ed25519.pub.toml redox/cookbook/repo/id_ed25519.pub.toml
